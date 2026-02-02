dcd03de693149f62d5bca9a32970e470b2b86285
Shubhankar Singh Salaria <100531997+ShubhankarSalaria@users.noreply.github.com>
2026-02-02 15:11:33 +0530

    m1 prac question

--- .net/day_5/class_library_calling/MyLibrary/obj/Debug/net10.0/MyLibrary.AssemblyInfo.cs
+++ .net/day_5/class_library_calling/MyLibrary/obj/Debug/net10.0/MyLibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90abd9d6027c547a68dee42af0aad571e91bcc87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+daaf50679ba235a00216d7612f19d527f4202351")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
